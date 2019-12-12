eb1fd2726cdf85ae73efbbfa2a85279a6ba5d952
Student <example@example.com>
2019-12-12 11:56:14 -0800

    update valuse to hundos

--- lib/coin_counter.rb
+++ lib/coin_counter.rb
@@ -13,8 +13,8 @@ class COIN_COUNTER
 
   def converter()
 
-    value_array = [500, 100, 25, 10, 5, 1]
-    name_array = ["fiver", "dollar", "quarter", "dime", "nickle", "penny"]
+    value_array = [10000, 2000, 1000, 500, 100, 25, 10, 5, 1]
+    name_array = ["hundo", "twenty", "ten", "fiver", "dollar", "quarter", "dime", "nickle", "penny"]
     @@new_vaule = cents
 
     value_array.each_with_index do |value, index|
@@ -38,5 +38,8 @@ class COIN_COUNTER
   end
 end
 
-test = COIN_COUNTER.new(687)
+puts "how many pennies do you have?"
+user_input = gets.chomp.to_i
+test = COIN_COUNTER.new(user_input)
+puts "Thats: "
 puts test.converter()
